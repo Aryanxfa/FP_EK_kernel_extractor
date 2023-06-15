b27ecd19246c52bc2a3daa5ec8a8b20c457917b3
Aryanxfa <aryan200152@gmail.com>
2023-06-15 22:20:02 +0530

    copy changelog to keep track

--- extract_eureka.sh
+++ extract_eureka.sh
@@ -2,15 +2,19 @@
 
 # List of folders where the script will run
 folders=("a10" "a20" "a20e" "a30" "a30s" "a40")
+directory=eureka_zip/
 
 # Loop through each folder
 for folder in "${folders[@]}"; do
     mkdir -p "$folder" &
 done
 
-./internal_extract_zips.sh eureka_zip/
+ls $directory >../version
 
-cd eureka_zip/
+# Loop through Extract each folder
+./internal_extract_zips.sh $directory
+
+cd $directory
 
 # Loop through each folder
 for folder in "${folders[@]}"; do
@@ -18,3 +22,6 @@ for folder in "${folders[@]}"; do
     mv *$folder-*"/dtb/oneui3/perm/5/dtb.img" "../$folder/"
     mv *$folder-*"/kernel/oneui4/Image" "../$folder/"
 done
+cd ..
+
+cp $directory/*a10*/META-INF/com/google/android/aroma/changelog.txt .
