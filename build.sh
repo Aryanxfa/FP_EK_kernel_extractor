9a62ab989f30207f14413d0e34be031990dfdcd2
Aryanxfa <aryan200152@gmail.com>
2023-07-17 18:37:50 +0530

    update for ksu kernels

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 # List of folders where the script will run
 folders=("a10" "a20" "a20e" "a30" "a30s" "a40")
 directory=eureka_zip/
+directory_ksu=eureka_zip_ksu/
+
+mkdir $directory
+mkdir $directory_ksu
 
 # Loop through each folder
 for folder in "${folders[@]}"; do
@@ -15,6 +19,7 @@ cp $directory/*a10*/META-INF/com/google/android/aroma/changelog.txt .
 
 # Loop through Extract each folder
 ./internal_extract_zips.sh $directory
+./internal_extract_zips.sh $directory_ksu
 
 # Loop through each folder
 cd $directory
@@ -27,8 +32,24 @@ cd ..
 
 for folder in "${folders[@]}"; do
     cd $folder
-    echo 
+    echo
     echo Packing $folder...
-    ../ramdisk_integrate.sh
+    ../ramdisk_integrate.sh boot.img
+    cd ..
+done
+
+cd $directory_ksu
+for folder in "${folders[@]}"; do
+    mv *$folder-*"/dtbo/dtbo.img" "../$folder/"
+    mv *$folder-*"/dtb/oneui3/perm/5/dtb.img" "../$folder/"
+    mv *$folder-*"/kernel/oneui4/Image" "../$folder/"
+done
+cd ..
+
+for folder in "${folders[@]}"; do
+    cd $folder
+    echo
+    echo Packing $folder...
+    ../ramdisk_integrate.sh ksu.img
     cd ..
 done
--- ramdisk_integrate.sh
+++ ramdisk_integrate.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Out file name not provided"
+    exit
+fi
+
 if [ ! -f ../ramdisk.cpio ]; then
-    echo "Failed with error 1"
+    echo "Ramdisk Missing"
     exit
 fi
 mv Image kernel
-rm boot.img
+rm $1
 cp ../dummy_boot.img ./
 cp ../header ./
 cp ../ramdisk.cpio ./
 
-magiskboot repack dummy_boot.img boot.img
+magiskboot repack dummy_boot.img $1
 
 rm header kernel dummy_boot.img ramdisk.cpio
 chmod 644 *
